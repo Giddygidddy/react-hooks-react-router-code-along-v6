26fa246c7caa01082f9f16000d4df8e049a1038c
Zilla <gideon.kariuki@student.moringaschool.com>
2024-08-14 16:18:04 +0300

    completed lab

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+function About() {
+    return (
+      <>
+        <header>
+         <Navbar />
+        </header>
+        <main>
+          <h1>This is my about component!</h1>
+        </main>
+      </>
+    );
+  };
+  
+  export default About;
+  
