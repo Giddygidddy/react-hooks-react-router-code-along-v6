efe3f6209e49ed19488fb23f300f517f59b96663
Zilla <gideon.kariuki@student.moringaschool.com>
2024-08-18 16:33:06 +0300

    completed labs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// Step 1. Import react-router functions
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.js";
 
 const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
